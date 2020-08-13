4b180acfafa907e4825847a3ca8721e9e8b4ea23
Duduxs <duduxss3@gmail.com>
2020-08-13 15:33:20 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/edudev/workshop/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.edudev.workshop.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edudev.workshop.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}

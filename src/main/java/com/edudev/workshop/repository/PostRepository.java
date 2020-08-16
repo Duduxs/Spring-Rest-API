0c389239bbf48efd1cceb0ee8dcd6ddbdbe74441
Duduxs <duduxss3@gmail.com>
2020-08-16 16:29:49 -0300

    Simples request with @Query

--- src/main/java/com/edudev/workshop/repository/PostRepository.java
+++ src/main/java/com/edudev/workshop/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.edudev.workshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.edudev.workshop.domain.Post;
@@ -10,5 +11,8 @@ import com.edudev.workshop.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': {$regex: ?0, $pattern: '$options': 'i' } }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
